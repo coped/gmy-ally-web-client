fd573449f4398c13105520ceb4c2609808332a12
Dennis Cope <dennisaaroncope@gmail.com>
2020-03-16 23:49:29 -0700

    Restructure component files

---
+++ src/components/App/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from '../../logo_transparent.png';
+import './App.css';
+
+class App extends Component {
+  componentDidMount() {
+    fetch("/api/v1/users.json")
+    .then(response => response.json())
+    .then(myJson => console.log(myJson))
+    .catch(error => console.error('Error:', error));
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Gym Partner is currently under construction.
+          </p>
+          <p>
+            Visit our <a href="https://github.com/coped/gym-partner-web-client">
+              <u>Github</u>
+            </a> for updates.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
